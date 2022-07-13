cc79544fa5c324acec82c58d02fe71b98e6d326a
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-07-13 00:13:35 -0500

    update App.js
    initiate project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tinder clone 2022 project
+
+A web app which exactly functions like the real Tinder. This is a clone coding project with MERN full stack project.
+
+All written by Jiuk Choi
---
+++ tinder-clone/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    //BEM class naming convention
+    <div className="App">
+      <h1>MERN Tinder project</h1>
+      {/* {Header} */}
+      {/* {TinderCards} */}
+      {/* {SwipeButtons} */}
+      
+    </div>
+  );
+}
+
+export default App;
